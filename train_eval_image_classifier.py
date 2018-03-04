0fd6f087367584dfa7f60bdb150982ec6832a1b2
HataFeng <fbjwying2@163.com>
2018-03-04 15:45:41 +0800

    change working dir for tinymind

--- train_eval_image_classifier.py
+++ train_eval_image_classifier.py
@@ -5,10 +5,24 @@ from __future__ import print_function
 import argparse
 import os
 
+def parse_args(check=True):
+    parser = argparse.ArgumentParser()
+    # train
+    parser.add_argument('--learning_rate', type=float, default=0.001)
+
+    FLAGS, unparsed = parser.parse_known_args()
+    return FLAGS, unparsed
+
 train_cmd = 'python ./train_image_classifier.py  --dataset_name={dataset_name} --dataset_dir={dataset_dir}  --model_name={model_name} --train_dir={train_dir} --learning_rate={learning_rate} --learning_rate_decay_factor={learning_rate_decay_factor}  --num_epochs_per_decay={num_epochs_per_decay} --weight_decay={weight_decay} --optimizer={optimizer} --batch_size={batch_size} --max_number_of_steps={max_number_of_steps} --clone_on_cpu={clone_on_cpu}'
 eval_cmd = 'python ./eval_image_classifier.py --dataset_name={dataset_name} --dataset_dir={dataset_dir} --dataset_split_name={dataset_split_name} --model_name={model_name}   --checkpoint_path={checkpoint_path}  --eval_dir={eval_dir} --batch_size={batch_size}  --max_num_batches={max_num_batches}'
 
 if __name__ == '__main__':
+    FLAGS, unparsed = parse_args()
+    print('current working dir [{0}]'.format(os.getcwd()))
+    w_d = os.path.dirname(os.path.abspath(__file__))
+    print('change wording dir to [{0}]'.format(w_d))
+    os.chdir(w_d)
+
     train_dir = '/output/ckpt72'
     dataset_name = 'quiz'
     dataset_dir = '/data/ai100/quiz-w7'
@@ -17,7 +31,7 @@ if __name__ == '__main__':
     max_number_of_steps = 1000
     batch_size = 32
     optimizer = 'sgd'
-    learning_rate = 0.1
+    learning_rate = FLAGS.learning_rate
     learning_rate_decay_factor = 0.1
     num_epochs_per_decay = 200
     clone_on_cpu = False
